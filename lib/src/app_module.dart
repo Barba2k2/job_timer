555b1ea86e658474deaeafc089bf487a636d1107
Barba2k2 <lorenzo.dz@hotmail.com>
2024-03-26 10:06:07 -0300

    FEAT: Add page routes on app module

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'modules/home/home_module.dart';
+import 'modules/login/login_module.dart';
 import 'modules/splash/splash.page.dart';
 
 class AppModule extends Module {
@@ -8,6 +10,8 @@ class AppModule extends Module {
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const SplashPage()),
+        ChildRoute('/', child: (_, __) => const SplashPage()),
+        ModuleRoute('/login', module: LoginModule()),
+        ModuleRoute('/home', module: HomeModule()),
       ];
 }
