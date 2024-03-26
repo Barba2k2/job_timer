3f287d9909f5e75797487b203fb18359eddf0b33
Barba2k2 <lorenzo.dz@hotmail.com>
2024-03-26 11:43:14 -0300

    FEAT: Created initial login page, login method is not implemented yet

--- lib/src/modules/login/login_page.dart
+++ lib/src/modules/login/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,11 +6,63 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeOf = MediaQuery.sizeOf(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF0092B9),
+              Color(0xFF0167B2),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              SizedBox(
+                height: sizeOf.height * .02,
+              ),
+              SizedBox(
+                width: sizeOf.width * .8,
+                height: 50,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.grey[200],
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Image.asset('assets/images/google.png'),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        'Google',
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
