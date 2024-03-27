89b3c36b388c509f6814dc5911152a0b77269fbe
Barba2k2 <lorenzo.dz@hotmail.com>
2024-03-27 11:36:44 -0300

    FEATÇ Created Login controller and sate for user login and verify login state and show right messages

---
+++ lib/src/modules/login/controller/login_controller.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: unused_field, prefer_final_fields
+
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:job_timer/src/services/auth/auth_service.dart';
+
+part 'login_state.dart';
+
+class LoginController extends Cubit<LoginState> {
+  AuthService _authService;
+
+  LoginController({required AuthService authService})
+      : _authService = authService,
+        super(
+          const LoginState.initial(),
+        );
+
+  Future<void> signIn() async {
+    try {
+      emit(
+        state.copyWith(
+          status: LoginStatus.loading,
+        ),
+      );
+      await _authService.singIn();
+    } catch (e, s) {
+      log('\nErro ao realizar login\n', error: e, stackTrace: s);
+      emit(
+        state.copyWith(
+          status: LoginStatus.failure,
+          errorMessage: 'Erro ao realizar login',
+        ),
+      );
+    }
+  }
+}
