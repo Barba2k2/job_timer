981afc8bd042d3bb37d849a4d3260a149d485bed
Barba2k2 <lorenzo.dz@hotmail.com>
2024-03-27 11:35:19 -0300

    FEATÇ Added binding to LoginModule class

--- lib/src/modules/login/login_module.dart
+++ lib/src/modules/login/login_module.dart
@@ -1,13 +1,25 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:job_timer/src/modules/login/login_page.dart';
+import 'package:modular_bloc_bind/modular_bloc_bind.dart';
+
+import 'controller/login_controller.dart';
 
 class LoginModule extends Module {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        BlocBind.lazySingleton(
+          //$ AuthService -> AppModule
+          (i) => LoginController(authService: i()),
+        ),
+      ];
 
   @override
   List<ModularRoute> get routes => [
-        ChildRoute('/', child: (context, args) => const LoginPage()),
-        
+        ChildRoute(
+          '/',
+          child: (context, args) => LoginPage(
+            controller: Modular.get(),
+          ),
+        ),
       ];
 }
