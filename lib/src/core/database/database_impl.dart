905207487ebf280882cd1d5be30a38ecce956540
Barba2k2 <lorenzo.dz@hotmail.com>
2024-04-02 12:59:59 -0300

    Added schemas for database connection

--- lib/src/core/database/database_impl.dart
+++ lib/src/core/database/database_impl.dart
@@ -1,9 +1,11 @@
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
+import '../../entities/project.dart';
+import '../../entities/project_task.dart';
 import './database.dart';
 
-class DatabaseImpl extends Database {
+class DatabaseImpl implements Database {
   Isar? _databaseInstance;
 
   @override
@@ -11,12 +13,11 @@ class DatabaseImpl extends Database {
     if (_databaseInstance == null) {
       final dir = await getApplicationSupportDirectory();
       _databaseInstance = await Isar.open(
-        schemas: [],
+        [ProjectSchema, ProjectTaskSchema],
         directory: dir.path,
         inspector: true,
       );
     }
-
     return _databaseInstance!;
   }
 }
