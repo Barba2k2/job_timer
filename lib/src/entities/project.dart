ea3537bbd4a0bea12ccef3cfde8ee414698cf53a
Barba2k2 <lorenzo.dz@hotmail.com>
2024-04-02 01:23:45 -0300

    FEAT: Created project schema

---
+++ lib/src/entities/project.dart
@@ -0,0 +1,19 @@
+import 'package:isar/isar.dart';
+import 'package:job_timer/src/entities/converters/project_status_converter.dart';
+import 'package:job_timer/src/entities/project_status.dart';
+
+import 'project_task.dart';
+
+// part 'project.g.dart';
+
+@Collection()
+class Project {
+  @Id()
+  int? id;
+
+  late String name;
+  @ProjectStatusConverter()
+  late ProjectStatus status;
+
+  final tasks = IsarLinks<ProjectTask>();
+}
